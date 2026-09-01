5fe86d5055312760c6580c86eab03f5e8d1ac2f2
Simon J Stuart <lakraven@gmail.com>
2026-09-01 18:21:41 +0200

    refactor: make Serial logging header a sink integration surface

--- src/ESPressio_Logging.hpp
+++ src/ESPressio_Logging.hpp
@@ -1,6 +1,5 @@
 #pragma once
-#include "ESPressio_DiagnosticTypes.hpp"
-#include "logging/ESPressio_ILoggerSink.hpp"
+
+#include <ESPressio_Logging.hpp>
+
 #include "logging/ESPressio_SerialLogSink.hpp"
-#include "logging/ESPressio_DiagnosticRingBuffer.hpp"
-#include "logging/ESPressio_Logger.hpp"
